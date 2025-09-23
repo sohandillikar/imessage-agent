e800ba81ee35185d4cab45b0f8a4e0d38f9d7fd9
Sohan Dillikar <sohanyt.main@gmail.com>
2025-09-22 17:14:49 -0700

    Created get_tools_list()

--- tools.py
+++ tools.py
@@ -1,21 +1,27 @@
+import os
+import utils
 from datetime import datetime
-from typing import Any
 
 # https://platform.openai.com/docs/guides/function-calling
 # If your function has no return value (e.g. send_email),
 # simply return a string to indicate success or failure. (e.g. "success")
 
+file_path = f"./{os.path.basename(__file__)}"
+tools = utils.get_tools_list(file_path, avoid_functions=["call_function"], web_search=True)
+
 def get_current_date() -> str:
     """
     Get the current date.
-    Example: Friday September 20, 2025
+    Returns:
+        str: The current date formatted as "Friday September 20, 2025"
     """
     return datetime.now().strftime("%A %B %d, %Y")
 
 def get_current_time() -> str:
     """
     Get the current time.
-    Example: 10:00 AM
+    Returns:
+        str: The current time formatted as "10:00 AM"
     """
     return datetime.now().strftime("%I:%M %p")
 
@@ -26,34 +32,3 @@ def call_function(name: str, args: dict):
         return get_current_time(**args)
     else:
         raise ValueError(f"Function {name} not found")
-
-tools = [
-    {
-        "type": "function",
-        "name": "get_current_date",
-        "description": "Get the current date.\nExample: Friday September 20, 2025",
-        "parameters": {
-            "type": "object",
-            "properties": {},
-            "required": []
-        }
-    },
-    {
-        "type": "function",
-        "name": "get_current_time",
-        "description": "Get the current time.\nExample: 10:00 AM",
-        "parameters": {
-            "type": "object",
-            "properties": {},
-            "required": []
-        }
-    },
-    {
-        "type": "web_search",
-        "user_location": {
-            "type": "approximate",
-            "country": "US",
-            "timezone": "America/Los_Angeles"
-        }
-    }
-]
