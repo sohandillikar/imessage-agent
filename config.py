f611a04cc5c7fa831c8a5e4c66a43bde557184db
Sohan Dillikar <sohanyt.main@gmail.com>
2025-09-22 19:08:25 -0700

    Added more contacts for testing

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
+import json
+
+def get_user_info():
+    with open("./knowledgebase/contacts.json", "r") as f:
+        contacts = json.load(f)
+    for contact in contacts:
+        if "self" in contact["relation"]:
+            return contact
+    return None
+
 MAC_USER = "sohan"
+USER = get_user_info()
